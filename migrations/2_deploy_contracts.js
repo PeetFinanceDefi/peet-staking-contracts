15908d9c23f828b2bd8273ffa5f8e6fcc86b1a84
w0dm4n <w0dm4n@hotmail.fr>
2021-01-06 23:44:54 +0100

    build abi && migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const PeetStakingContract = artifacts.require("PeetStakingContract");
 
 module.exports = function(deployer) {
-  deployer.deploy(PeetStakingContract, "0x8984e422E30033A84B780420566046d25EB3519a");
+  deployer.deploy(PeetStakingContract, "0x03a2c6731A70611ffC486207129DD3f8DECb0a65");
 };
